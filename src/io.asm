15cf95b2a12f1ccd1c509b27249d5988a6fa7173
Carlos Rabelo <developer@carlosrabelo.com.br>
2014-02-04 15:23:00 -0300

    feat(io): add PRINT_STR, PRINT_NUMBER and PRINT_CRLF routines

--- src/io.asm
+++ src/io.asm
@@ -36,3 +36,49 @@ OUTCHAR:
     li      $v0, 11         # MARS syscall 11: Print Character
     syscall                 # Execute syscall. Prints character in $a0.
     jr      $ra             # Return to caller
+
+# -----------------------------------------------------------------------
+# PRINT_STR - Print a null-terminated string
+# -----------------------------------------------------------------------
+# Description:
+#   Prints the null-terminated string located at address $a0.
+#
+# Input: $a0 (Address of string)
+# Output: None
+# Clobbers: $v0
+# -----------------------------------------------------------------------
+PRINT_STR:
+    li      $v0, 4          # MARS syscall 4: Print String
+    syscall
+    jr      $ra
+
+# -----------------------------------------------------------------------
+# PRINT_NUMBER - Print an integer
+# -----------------------------------------------------------------------
+# Description:
+#   Prints the 32-bit integer passed in $a0.
+#
+# Input: $a0 (Integer to print)
+# Output: None
+# Clobbers: $v0
+# -----------------------------------------------------------------------
+PRINT_NUMBER:
+    li      $v0, 1          # MARS syscall 1: Print Integer
+    syscall
+    jr      $ra
+
+# -----------------------------------------------------------------------
+# PRINT_CRLF - Print a newline
+# -----------------------------------------------------------------------
+# Description:
+#   Prints a CR/LF (newline) sequence to the console.
+#
+# Input: None
+# Output: None
+# Clobbers: $v0, $a0
+# -----------------------------------------------------------------------
+PRINT_CRLF:
+    la      $a0, STR_CRLF
+    li      $v0, 4          # MARS syscall 4: Print String
+    syscall
+    jr      $ra
